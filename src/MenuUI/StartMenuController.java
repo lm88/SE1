f2e0650f254c6ebb96416cf213e2ed69ae0dabc3
TABLET-BECKY\Becky <rme9@students.uwf.edu>
2017-11-13 18:31:16 -0600

    Fixed some things that got messed up when uploading to GitHub for the first time.

--- src/MenuUI/StartMenuController.java
+++ src/MenuUI/StartMenuController.java
@@ -18,27 +18,21 @@ public class StartMenuController {
 	@FXML private void transitionView(ActionEvent event) {
 		// id button caller
 		String btn = ((Button) event.getSource()).getId();
-		String selection;
-		NavigationController.PREVIOUS = NavigationController.STARTMENU;
+                String previous = NavigationController.PREVIOUS;
+		NavigationController.PREVIOUS = NavigationController.SAVELOAD;
 		// send load request
-                if (btn.startsWith("save"))
-                        selection = btn;
-		if (btn.equals("save"))
-			// Save the game, stay on this view
-                        saveGame(btn);
-                if (btn.equals("load"))
-                        // Load the game, trasition to main menu
-                        
+		if (btn.equals("back"))
+			NavigationController.loadView(previous);
 		if (btn.equals("loadgame"))
 			NavigationController.loadView(NavigationController.SAVELOAD);
 
 		// if this spot is reached: do nothing
 	}
     
-    private void saveGame(String fileName)
-    {
-        FileIO fileHandler = new FileIO();
         
-        fileHandler.saveFile(fileName.charAt(-1));
+        
+    /** Send exit request */
+	@FXML private void exit() {
+		NavigationController.closeApplication();
     }
 }
