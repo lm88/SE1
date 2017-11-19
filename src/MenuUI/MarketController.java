d20f49c5e056025f3dcc6c74f4c10d03316214ea
rme9 <rme9@students.uwf.edu>
2017-11-19 07:07:08 -0600

    Created MarketUI

---
+++ src/MenuUI/MarketController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MenuUI;
+
+import UIFramework.NavigationController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author becky
+ */
+public class MarketController {
+
+     private String _Previous;
+     private int UpgradeCost;
+    
+    /** Send view change request
+	 * @param event - the onAction event from fxml */ 
+	@FXML 
+	private void transitionView(ActionEvent event) {
+		// id button caller
+		String btn = ((Button) event.getSource()).getId();
+		
+		// send load request
+		switch(btn)
+		{
+		    case "back":
+			_Previous = NavigationController.PREVIOUS;
+			NavigationController.PREVIOUS = NavigationController.MARKET;
+			NavigationController.loadView(_Previous);
+			break;
+		    case "":
+			break;
+		    default:
+			break;
+		}
+		
+		// if this spot is reached: do nothing
+	}   
+    
+	@FXML
+	public void applyUpgrade()
+	{
+	    
+	}
+}
