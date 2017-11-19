33b5d66d406ca4bdd6e28c6fdcb2440e3861a35f
Kac86 <31518071+Kac86@users.noreply.github.com>
2017-11-19 11:17:50 -0600

    Add files via upload
    Here are the Player and Unit classes, modResource and getUnit are left blank for future tweaking

---
+++ Unit.java
@@ -0,0 +1,47 @@
+
+public class Unit {
+	public String type;
+	public int health;
+	public int resource;
+	public int damage; 
+	
+	public Unit(String newType, int newDamage) {
+		type = newType;
+		damage = newDamage; 
+	}
+	
+	public void setType(String newType) {
+		type = newType; 
+	}
+	public void setHealth(int newHealth) {
+		health = newHealth;
+	}
+	public void setResource(int newResource) {
+		resource = newResource;
+	}
+	public void setDamage(int newDamage) {
+		damage = newDamage;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public int getResource() {
+		return resource;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	
+	public void modHealth(int modBy) {
+		health = health - damage; 
+	}
+	
+	public void modResource(int modBy) {
+		
+	}
+}
+
