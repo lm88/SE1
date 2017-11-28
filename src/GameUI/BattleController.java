f5940d7814ebf1914b5faf9bcfb6cac38c11f616
lm88 <lm88@students.uwf.edu>
2017-11-28 12:10:01 -0600

    Added logo to title, started adding features to battle

--- src/GameUI/BattleController.java
+++ src/GameUI/BattleController.java
@@ -3,9 +3,22 @@ package GameUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
+import java.util.ArrayList;
+
+import DataModels.Player;
+import DataModels.Unit;
 import UIFramework.NavigationController;
 
 public class BattleController {
+	
+	@FXML Button playerUnit1;
+	@FXML Button playerUnit2;
+	@FXML Button playerUnit3;
+	
+	@FXML private void initialize() {
+		setUnitIcons();
+	}
 	
 	/** Send tile coordinates */
 	@FXML private void tilePress(ActionEvent event) {
@@ -20,4 +33,36 @@ public class BattleController {
 	@FXML private void transitionView() {
 		NavigationController.loadView(NavigationController.MAINMENU);
 	}
+	
+
+	   
+   private void setUnitIcons()
+   {
+       try
+       {
+	    playerUnit1.getStyleClass().add(determineIcon(0));
+	    playerUnit2.getStyleClass().add(determineIcon(1));
+	    playerUnit3.getStyleClass().add(determineIcon(2));
+       }
+       catch(Exception e)
+       {
+	   String m = e.getMessage();
+       }
+   }
+   
+   private String determineIcon(int index)
+   {
+       ArrayList<Unit> u = Player.unitList;
+       switch(u.get(index).type)
+       {
+	   case "water":
+	       return "waterUnit";
+	   case "fire":
+	       return "fireUnit";
+	   case "earth":
+	       return "earthUnit";
+	   default:
+	       return"";
+       }
+   }
 }
