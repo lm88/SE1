82335492662e68560fcfd1a744470fbaab21e442
KennethDale1980 <kd31@students.uwf.edu>
2017-11-28 01:17:20 -0600

    Changed entire fxml layout.  Deleted extra files I created.

--- src/GameUI/GameCreationController.java
+++ src/GameUI/GameCreationController.java
@@ -3,73 +3,54 @@ package GameUI;
  * @author Kenneth Dale
  *
  */
+import java.io.IOException;
+
 import DataModels.Player;
 import DataModels.Unit;
-import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import UIFramework.NavigationController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class GameCreationController {
-	@FXML
-	private Button teamMember1;
+	@FXML Button teamMember1, teamMember2, teamMember3;
 	
-	@FXML
-	private Button teamMember2;
-	
-	@FXML
-	private Button teamMember3;
-	
-	
+	@FXML HBox typeSelectContainer;
 	
 	Player thisPlayer = new Player();
-	TypeSelectController tsc = new TypeSelectController();
-	Stage stage;
 	int teamMemberIndex;
 	
 	@FXML
 	private void handlePlayerSelect(ActionEvent event) throws IOException {
 		//teamMemberIndex = Integer.parseInt(((Button) event.getSource()).getId());
+		typeSelectContainer.setVisible(true);
 		
-		tsc.displayModal();
 	}
 	
 	@FXML
 	private void handleTypeSelect(ActionEvent event) throws IOException {
 		String type = ((Button) event.getSource()).getId();
-		Button thisButton = (Button) event.getSource();
-		Stage currentStage = (Stage) thisButton.getScene().getWindow();
 		
 		switch(type) {
 			case "fire":
 				thisPlayer.unitList.add(teamMemberIndex, new Unit(type, 10));
-				currentStage.hide();
+				typeSelectContainer.setVisible(false);
 				break;
 			case "earth":
 				thisPlayer.unitList.add(teamMemberIndex, new Unit(type, 10));
-				currentStage.hide();
+				typeSelectContainer.setVisible(false);
 				break;
 			case "water":
 				thisPlayer.unitList.add(teamMemberIndex, new Unit(type, 10));
-				currentStage.hide();
+				typeSelectContainer.setVisible(false);
 				break;
+				
+			
 		}
 		
+		
 	}
-	
-	
 	
 	/** Send view change request */ 
 	@FXML private void transitionView() {
