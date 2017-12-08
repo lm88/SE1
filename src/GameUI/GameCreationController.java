fe6ec451664e9c61b042b51562b32353b407a0d9
lm88 <lm88@students.uwf.edu>
2017-12-08 01:29:05 -0600

    initialized currency as zero on new game creation

--- src/GameUI/GameCreationController.java
+++ src/GameUI/GameCreationController.java
@@ -29,6 +29,7 @@ public class GameCreationController {
 	public void initialize() {
 		Player.unitList.clear();  //Clear existing units in unitList
 		Player.level = 1;
+		Player.currency = 0;
 		for(int i = 0; i < 3; i++) {
 			Player.unitList.add(i, null);
 		}
